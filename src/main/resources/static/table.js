b4913a70b22e9bbccb9d55cf28e058af95362c4a
gkluz <gkluz@datagraphic.co.uk>
2022-10-27 14:32:22 +0100

    changing table logic(hopefully simplicity it)

--- src/main/resources/static/table.js
+++ src/main/resources/static/table.js
@@ -1,23 +1,59 @@
-function create(columnsNum, rowNum) {
+function create(columnsNum, rowNum, solid, header) {
+
+    let s;
+    let h;
+
+    if(solid==1){
+        s=" border=\"solid\""
+    }else{
+        s=""
+    };
+
+    if(header==1){
+        h=" font-weight=\"bold\""
+    }else{
+        h=""
+    };
+
+
+
+
 // creating row layout
     let columns = '\n\t\t<fo:table-column column-width=\"20mm\"/>';
     columns = columns.repeat(columnsNum);
 
+
+
     let rowBlock = '\n\t\t\t\t<fo:table-cell>' +
-        '\n\t\t\t\t\t<fo:block>' +
+        '\n\t\t\t\t\t<fo:block'+h+'>' +
         '\n\t\t\t\t\t\t #Text' +
         '\n\t\t\t\t\t</fo:block>' +
         '\n\t\t\t\t</fo:table-cell>'
+    let rows= ""
 
-    rowBlock = rowBlock.repeat(columnsNum)
+    for(let i=1;i<=rowNum;i++){
+       rows =rows +'\n\t\t\t<fo:table-row>'+ rowBlock;
+        for(j=1;j<=(columnsNum-1);j++){
+            rows+= rowBlock;
+        }
+        rows += '\n\t\t\t</fo:table-row>'
+
+    }
 
 
-//row layout
-    let rows = '\n\t\t\t<fo:table-row>' + rowBlock + '\n\t\t\t</fo:table-row>'
-    rows = rows.repeat(rowNum);
+
+//
+//     rowBlock = rowBlock.repeat(columnsNum)
+//
+//
+// //row layout
+//     let rows = '\n\t\t\t<fo:table-row>' + rowBlock + '\n\t\t\t</fo:table-row>'
+//     rows = rows.repeat(rowNum);
+//
+//
 
 
-    let tab = '<fo:block-container font-size=\"8.5pt\" border=\"solid\">' +
+    let tab = '<fo:block-container font-size=\"8.5pt\"'+s+'>' +
         '\n\t<fo:table>' +
         columns +
         '\n\t\t<fo:body>' +
@@ -32,15 +68,34 @@ return tab;
 function addFields(columnsNum){
     $(".colsize").html("");
     for(let x =1;x<=columnsNum;x++ ){
-    $('.colsize').append('<input type="text" id="colsize'+x+'"><br>');}
+    $('.colsize').append('<span>Column '+x+' width in % </span><input type="text" id="colsize'+x+'"><br>');}
 }
 
 
 $("#create").click(function (){
     let x =$('#col').val();
     let y = $('#row').val()
-    let sum = create(x,y);
-$('textarea.text').val(sum);
+    let s;
+    let h
+
+
+    let solid = document.getElementById("solid");
+    let header =$("#sheaders");
+
+    if (solid.checked==true){
+        s = 1
+    }else {
+        s = 0
+    }
+
+    if (header.checked==true){
+        h = 1
+    }else {
+        h = 0
+    }
+
+    let details = create(x,y,s,h);
+$('textarea.text').val(details);
 addFields(x);
 
 });
