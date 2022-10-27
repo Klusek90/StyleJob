933ccfe2a4b8e52890de4adcacbc82be6c365335
gkluz <gkluz@datagraphic.co.uk>
2022-10-27 12:17:47 +0100

    checkpoint

---
+++ src/main/resources/static/additional table.js
@@ -0,0 +1,9 @@
+// $("#create").click(function (){
+
+
+
+// $(".additional").html("");
+// for (let x =1;x<=columnsNum;x++ ){
+// $(".additional").append('<span>Column '+i+' width in %</span><input type="text" id="colsize'+i+'" class="colsize"><br>');
+    // $(".additional").append('<input type="text" class="colsize"><br>')
+// });
--- src/main/resources/static/table.js
+++ src/main/resources/static/table.js
@@ -1,36 +1,44 @@
-$("#but").click(function (){
-
-let columnsNum =$('#col').val();
-let rowNum=$('#row').val();
-
+function create(columnsNum, rowNum) {
 // creating row layout
-let columns = '\n\t\t<fo:table-column column-width=\"20mm\"/>';
-columns= columns.repeat(columnsNum);
+    let columns = '\n\t\t<fo:table-column column-width=\"20mm\"/>';
+    columns = columns.repeat(columnsNum);
 
-let rowBlock = '\n\t\t\t\t<fo:table-cell>'+
-                   '\n\t\t\t\t\t<fo:block>'+
-                        '\n\t\t\t\t\t\t #Text' +
-                    '\n\t\t\t\t\t</fo:block>'+
-                '\n\t\t\t\t</fo:table-cell>'
+    let rowBlock = '\n\t\t\t\t<fo:table-cell>' +
+        '\n\t\t\t\t\t<fo:block>' +
+        '\n\t\t\t\t\t\t #Text' +
+        '\n\t\t\t\t\t</fo:block>' +
+        '\n\t\t\t\t</fo:table-cell>'
 
-rowBlock =rowBlock.repeat(columnsNum)
+    rowBlock = rowBlock.repeat(columnsNum)
 
 
 //row layout
-let rows = '\n\t\t\t<fo:table-row>'+ rowBlock+'\n\t\t\t</fo:table-row>'
-rows =rows.repeat(rowNum);
+    let rows = '\n\t\t\t<fo:table-row>' + rowBlock + '\n\t\t\t</fo:table-row>'
+    rows = rows.repeat(rowNum);
 
 
-let tab = '<fo:block-container font-size=\"8.5pt\" border=\"solid\">'+
-            '\n\t<fo:table>'+
-                columns+
-                '\n\t\t<fo:body>'+
-                rows+
-                '\n\t\t</fo:body>'+
-            '\n\t</fo:table>'+
-            '\n</fo:block-container>';
+    let tab = '<fo:block-container font-size=\"8.5pt\" border=\"solid\">' +
+        '\n\t<fo:table>' +
+        columns +
+        '\n\t\t<fo:body>' +
+        rows +
+        '\n\t\t</fo:body>' +
+        '\n\t</fo:table>' +
+        '\n</fo:block-container>';
+
+return tab;
+}
+
+function addFields(){
+    $('.main').append('<input type="text" class="colsize"><br>');
+}
 
 
+$("#create").click(function (){
+    let x =$('#col').val();
+    let y = $('#row').val()
+    let sum = create(x,y);
+$('textarea.text').val(sum);
+console.log("pl");
 
-$('.text').val(tab);
 });
