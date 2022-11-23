bf71e588bc1e146db7bc3ed02ac9b334d7f2d2fb
gkluz <gkluz@datagraphic.co.uk>
2022-11-23 16:06:29 +0000

    adding new processes

--- src/main/resources/static/createJobStructure.js
+++ src/main/resources/static/createJobStructure.js
@@ -6,7 +6,11 @@ $("#create").click(function (){
         code += checkSequence(i,i)
     }
 
+    let wrap =document.getElementById("wraper");
+    if (wrap.checked == true)
+    {
     code = jobDesign(code)
+    }
     $('textarea.text').val(code)
 
 
@@ -14,6 +18,8 @@ $("#create").click(function (){
 })
 
 function checkSequence(child, position){
+
+    let name= $("#filename").val();
     let compare = $(".jobStructure p:nth-child("+child+")").html()
     let code
     switch (compare){
@@ -21,7 +27,7 @@ function checkSequence(child, position){
             code=   '\n\t\t<process>' +
                     '\n\t\t\t<position>' + position + '</position>' +
                     '\n\t\t\t<script>Autopilot</script>' +
-                    '\n\t\t\t<application>Appname</application>' +              //add app name
+                    '\n\t\t\t<application>'+name+'</application>' +              //add app name
                     '\n\t\t\t<extension>PS</extension>' +                       //add extenstion
                     '\n\t\t\t<quantity>count(/data/items/item)</quantity>'+
                     '\n\t\t</process>';
@@ -30,7 +36,7 @@ function checkSequence(child, position){
             code=   "\n\t\t<process spoils=\"true\">" +
                     "\n\t\t\t<position>"+position+"</position>" +
                     "\n\t\t\t<script>foppy.pl</script>" +
-                    "\n\t\t\t<dir>foppy/[path]</dir>" +
+                    "\n\t\t\t<dir>foppy/"+name+"</dir>" +
                     "\n\t\t\t<xsl>Letters.xsl</xsl>" +
                     "\n\t\t\t<duplex>0</duplex>"+
                     "\n\t\t\t<output-folder>1. data to run/xerox</output-folder>" +
@@ -47,7 +53,106 @@ function checkSequence(child, position){
                     "\n\t\t\t<position>"+position+"</position>" +
                     "\n\t\t\t<script>Chain</script>" +
                     "\n\t\t</process>";
+            break
+        case "Report":
+            code = "\n\t\t<process>" +
+                "\n\t\t\t<position>"+position+"</position>" +
+                "\n\t\t\t<script>report.pl</script>" +
+                "\n\t\t\t<extension>rpt</extension>" +
+                "\n\t\t\t<output-folder>1. data to run/reports</output-folder>" +
+                "\n\t\t\t<lines>" +
+                "\n\t\t\t\t<line><chunks><chunk>"+name+" Suppression Report</chunk></chunks></line>" +
+                "\n\t\t\t\t<line><chunks><chunk>-------------------------------------------------------------------------</chunk></chunks></line>" +
+                "\n\t\t\t\t<line/>" +
+                "\n\t\t\t\t<line><chunks><chunk>Input filename: </chunk><chunk><xpath>/data/metadata/filename</xpath></chunk></chunks></line>" +
+                "\n\t\t\t\t<line/>" +
+                "\n\t\t\t\t<line><chunks><chunk>Processed on: </chunk><chunk><xpath>/data/metadata/datestamp</xpath></chunk></chunks></line>" +
+                "\n\t\t\t\t<line><chunks><chunk>Processed at: </chunk><chunk><xpath>/data/metadata/timestamp</xpath></chunk></chunks></line>" +
+                "\n\t\t\t\t<line/>" +
+                "\n\t\t\t\t<line><chunks><chunk>Total records: </chunk><chunk><xpath>count(/data/items/item)</xpath></chunk></chunks></line>" +
+                "\n\t\t\t\t<line/>" +
+                "\n\t\t\t\t<line><chunks><chunk>Number of suppressed records: </chunk><chunk><xpath>count(/data/metadata/suppressions/suppressed)</xpath></chunk></chunks></line>" +
+                "\n\t\t\t\t<line/>" +
+                "\n\t\t\t\t<line><chunks><chunk>Suppressed records:</chunk></chunks></line>" +
+                "\n\t\t\t\t<line/>" +
+                "\n\t\t\t\t<line repeat-for-each=\"/data/metadata/suppressions/suppressed\">" +
+                "\n\t\t\t\t\t<chunks>" +
+                "\n\t\t\t\t\t\t<chunk>Account Name: </chunk><chunk><xpath>fields/field[name = 'Account']/value</xpath></chunk>" +
+                "\n\t\t\t\t\t\t<chunk> Account Number: </chunk><chunk><xpath>fields/field[name = 'Number']/value</xpath></chunk>" +
+                "\n\t\t\t\t\t\t<chunk> Post Code: </chunk><chunk><xpath>fields/field[name = 'PostCode']/value</xpath></chunk>" +
+                "\n\t\t\t\t\t</chunks>" +
+                "\n\t\t\t\t</line>" +
+                "\n\t\t\t\t<line/>" +
+                "\n\t\t\t\t</lines>" +
+                "\n\t\t</process>";
+            break
+        case "Mailcheck":
+            code= "\n\t\t<process>\n" +
+                    "\t\t\t<position>"+position+"</position>\n" +
+                    "\t\t\t<script>mailcheck.pl</script>\n" +
+                    "\t\t\t<output-folder>1. data to run/archive</output-folder>\n" +
+                    "\t\t\t<fields>\n" +
+                    "\t\t\t\t<field>\n" +
+                    "\t\t\t\t\t<position>1</position>\n" +
+                    "\t\t\t\t\t<xpath>/data/metadata/filename</xpath>\n" +
+                    "\t\t\t\t</field>\n" +
+                    "\t\t\t\t<field>\n" +
+                    "\t\t\t\t\t<position>2</position>\n" +
+                    "\t\t\t\t\t<xpath>field4</xpath>\n" +
+                    "\t\t\t\t</field>\n" +
+                    "\t\t\t\t<field>\n" +
+                    "\t\t\t\t\t<position>3</position>\n" +
+                    "\t\t\t\t\t<xpath>field5</xpath>\n" +
+                    "\t\t\t\t</field>\n" +
+                    "\t\t\t\t<field>\n" +
+                    "\t\t\t\t\t<position>4</position>\n" +
+                    "\t\t\t\t\t<xpath>field6</xpath>\n" +
+                    "\t\t\t\t</field>\n" +
+                    "\t\t\t\t<field>\n" +
+                    "\t\t\t\t\t<position>5</position>\n" +
+                    "\t\t\t\t\t<xpath>field7</xpath>\n" +
+                    "\t\t\t\t</field>\n" +
+                    "\t\t\t\t<field>\n" +
+                    "\t\t\t\t\t<position>6</position>\n" +
+                    "\t\t\t\t\t<xpath>field8</xpath>\n" +
+                    "\t\t\t\t</field>\n" +
+                    "\t\t\t\t<field>\n" +
+                    "\t\t\t\t\t<position>7</position>\n" +
+                    "\t\t\t\t\t<xpath>field9</xpath>\n" +
+                    "\t\t\t\t</field>\n" +
+                    "\t\t\t\t<field>\n" +
+                    "\t\t\t\t\t<position>8</position>\n" +
+                    "\t\t\t\t\t<xpath>field10</xpath>\n" +
+                    "\t\t\t\t</field>\n" +
+                    "\t\t\t</fields>\n" +
+                    "\t\t</process>"
             ;break
+        case "PostScript-Xerox-Opt":
+            code = "\t\t<process spoils=\"true\">\n" +
+                    "\t\t\t<position>3</position>\n" +
+                    "\t\t\t<script>postscript-xerox-opt.pl</script>\n" +
+                    "\t\t\t<dir>"+name+"</dir>\n" +
+                    "\t\t\t<xsl>"+name+".xsl</xsl>\n" +
+                    "\t\t\t<output-folder>1. data to run/xerox</output-folder>\n" +
+                    "\t\t</process>"
+
+            ;break
+
+        case "LookUp":
+            code = "\n\t\t<process>\n" +
+                    "\t\t\t<position>4</position>\n" +
+                    "\t\t\t<script>lookup-palmer.pl</script>\n" +
+                    "\t\t\t<lookup>[look up file name]</lookup>\n" +
+                    "\t\t\t<lookup-entry>/data/items/item</lookup-entry>\n" +
+                    "\t\t\t<lookup-field>[accountID]</lookup-field>\n" +
+                    "\t\t\t<match-field>[accountnumber]</match-field>\n" +
+                    "\t\t\t<fields>\n" +
+                    "\t\t\t\t<field>\n" +
+                    "\t\t\t\t\t<position>1</position>\n" +
+                    "\t\t\t\t\t<xpath>sheet</xpath>\n" +
+                    "\t\t\t\t</field>\n" +
+                    "\t\t\t</fields>\n" +
+                    "\t\t</process>"
         default: code=""; break
 
     }
