500f119c59322fd9e93d6ab5f18f4586b19fc737
gkluz <gkluz@datagraphic.co.uk>
2023-02-03 15:01:08 +0000

    download button

--- src/main/resources/static/job/download.js
+++ src/main/resources/static/job/download.js
@@ -3,12 +3,17 @@ document.getElementById('downloadXML').onclick = function (){
     let name = document.getElementById('filename').value;
     let string= document.getElementById('content').value;
 
-    let data = "data:text/xml;charset=utf-8" + encodeURIComponent(string);
-    console.log(data);
-    let anchor = document.createElement('a');
-    anchor.setAttribute("href",data);
-    anchor.setAttribute("download",name+".xml");
-    anchor.click();
-    anchor.remove();
+    const blob = new Blob([string],{type:"octet-steam"})
 
+    const href = URL.createObjectURL(blob)
+
+    const a =Object.assign(document.createElement("a"), {
+        href,
+        style: "display:none",
+        download: name+".xml",
+    });
+    document.body.appendChild(a)
+    a.click()
+    URL.revokeObjectURL(href)
+    a.remove();
 }
